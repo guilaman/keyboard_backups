30bb5b973299873ed9c1cb1f05ddd749f38e7bfd
Guillermo Amarante <gac317@gmail.com>
2023-09-21 20:26:02 +0800

    Improve navigation keys on Minila

--- dz_minila67/minila2mini/keymaps/minila2mini/keymap.c
+++ dz_minila67/minila2mini/keymaps/minila2mini/keymap.c
@@ -47,8 +47,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_BSPC, KC_F1,  KC_F2,    KC_F3,      KC_F4,    KC_F5,  KC_F6,    KC_F7,      KC_F8,  KC_F9,  KC_F10,      KC_F11 , KC_F12,_______, KC_DEL,
       KC_ENT  ,KC_DEL , KC_P7 ,    KC_P8 ,     KC_P9 ,  KC_PPLS,KC_PAST,  KC_PGUP,    KC_UP, KC_PGDN,  KC_HOME,     _______, _______,     KC_ESC ,
       CAPSWRD   , KC_PDOT, KC_P4 ,   KC_P5 ,     KC_P6 ,  KC_PMNS,KC_PSLS,  KC_LEFT,    KC_DOWN, KC_RGHT,  KC_END,         KC_NLCK,     KC_TAB   ,
-      DM_REC1     ,  KC_P0 , KC_P1 ,   KC_P2 ,     KC_P3 ,  KC_PEQL,_______,  KC_WH_D,     KC_WH_U,KC_WH_L, KC_WH_R,      KC_SLCK,_______, KC_INS,
-      DM_PLY1    ,    KC_APP ,  _______,        MO(3) ,            _______       ,   _______,        _______,_______,     _______,_______,_______),
+      DM_REC1     ,  KC_P0 , KC_P1 ,   KC_P2 ,     KC_P3 ,  KC_PEQL,_______,  KC_WH_D,     KC_WH_U,KC_WH_L, KC_WH_R,      KC_SLCK,KC_HOME, KC_INS,
+      DM_PLY1    ,    KC_APP ,  _______,        MO(3) ,            _______       ,   _______,        _______,_______,     KC_PGUP, KC_END,KC_PGDN),
 /* *** *** *** *** *** *** *** *** *** */
 
 
@@ -63,10 +63,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KEYMAP(
 
     KC_BSPC,  KC_F1,  KC_F2,    KC_F3,      KC_F4,    KC_F5,  KC_F6,    KC_F7,      KC_F8,  KC_F9,  KC_F10,      KC_F11 , KC_F12, _______,KC_DEL,
-    KC_ENT  ,KC_DEL, _______,   _______,    _______,  _______,_______,  KC_BTN1,  KC_MS_U, KC_BTN2,  _______,     _______, _______,      KC_ESC ,
-    CAPSWRD   , _______,_______,  _______,    _______,  _______,_______,  KC_MS_L,    KC_MS_D, KC_MS_R, _______,         KC_NLCK,      KC_TAB   ,
-    DM_REC2     , KC_WH_L,KC_WH_R, KC_WH_U,    KC_WH_D,  KC_CALC,_______,  _______,     _______,_______, _______,        KC_SLCK,_______, KC_INS,
-    DM_PLY2    ,  KC_PSCR,  _______,        _______,            _______   ,       MO(3)  ,        _______,_______,      _______,_______,_______),
+    KC_ENT  ,KC_HOME, KC_PGUP,   KC_UP,     KC_PGDN,  _______,_______,  KC_BTN1,  KC_MS_U, KC_BTN2,  _______,     _______, _______,      KC_ESC ,
+    KC_DEL   , KC_END, KC_LEFT,  KC_DOWN,     KC_RGHT,  _______,_______,  KC_MS_L,    KC_MS_D, KC_MS_R, _______,         KC_NLCK,      KC_TAB   ,
+    DM_REC2     , KC_WH_L,KC_WH_R, KC_WH_U,    KC_WH_D,  KC_CALC,_______,  _______,     _______,_______, _______,        KC_SLCK,KC_HOME, KC_INS,
+    DM_PLY2    ,  KC_PSCR,  _______,        _______,            _______   ,       MO(3)  ,        _______,_______,      KC_PGUP, KC_END,KC_PGDN),
 /* *** *** *** *** *** *** *** *** *** */
 
 
@@ -79,10 +79,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      ____        ,    ____ ,   ____  ,        {____} ,          ____            ,   {____},         ____  , ____ ,         ____,   ____ ,  ____ .*/
 	KEYMAP(
     MO(5)  ,_______,_______, _______,     _______,  _______,_______,  _______,    _______,_______,_______,      _______,_______,_______,_______,
-    _______ ,_______,RGB_HUD,  RGB_VAI,     RGB_HUI,  _______,_______,  KC_MPLY,    KC_VOLU,_______,_______,      _______, _______,   _______  ,
-    _______   , _______,RGB_SAD, RGB_VAD,     RGB_SAI,  RGB_TOG,_______,  KC_MRWD,    KC_VOLD,KC_MFFD,_______,         _______,      _______   ,
-    _______     , _______,RGB_MOD, RGB_M_P,     RGB_M_SW,  _______,_______, _______,    _______,_______, _______,      _______,KC_BRIU, _______,
-    _______    ,  _______,  _______,        _______,             TG(4)       ,  _______,        _______,_______,     _______, KC_BRID, _______),
+    _______ ,_______,RGB_HUD,  RGB_VAI,     RGB_HUI,  _______,_______,  KC_MPLY,    KC_BRIU,_______,_______,      _______, _______,   _______  ,
+    _______   , _______,RGB_SAD, RGB_VAD,     RGB_SAI,  RGB_TOG,_______,  KC_VOLD,    KC_BRID,KC_VOLU,_______,         _______,      _______   ,
+    _______     , _______,RGB_MOD, RGB_M_P,     RGB_M_SW,  _______,_______, _______,    _______,_______, _______,      KC_MPLY,KC_VOLU, _______,
+    _______    ,  _______,  _______,        _______,             TG(4)       ,  _______,        _______,_______,     KC_MRWD, KC_VOLD, KC_MFFD),
 /* *** *** *** *** *** *** *** *** *** */
 
 
@@ -97,8 +97,8 @@ KEYMAP(
     _______,_______,_______, _______,     _______,  _______,_______,  _______,    _______,_______,_______,      _______,_______,_______,_______,
     _______ ,KC_HOME,KC_PGUP,  KC_UP,     KC_PGDN,  _______,_______,  KC_PGUP,    KC_UP, KC_PGDN,  KC_HOME,      _______, _______,   _______  ,
     _______   , KC_END,KC_LEFT, KC_DOWN,     KC_RGHT,  _______,_______,  KC_LEFT,    KC_DOWN, KC_RGHT,  KC_END,         _______,      _______   ,
-    _______     , KC_WH_L,KC_WH_R, KC_WH_U,    KC_WH_D,  KC_SLCK,KC_SLCK,  KC_WH_D,     KC_WH_U,KC_WH_L, KC_WH_R,      _______,_______, _______,
-    _______    ,  _______,  _______,        _______,            _______      ,  _______,        _______,_______,     _______, _______, _______),
+    _______     , KC_WH_L,KC_WH_R, KC_WH_U,    KC_WH_D,  KC_SLCK,KC_SLCK,  KC_WH_D,     KC_WH_U,KC_WH_L, KC_WH_R,      KC_PGUP,KC_WH_U, KC_PGDN,
+    _______    ,  _______,  _______,        _______,            _______      ,  _______,        _______,_______,     KC_WH_L, KC_WH_D, KC_WH_R),
 /* *** *** *** *** *** *** *** *** *** */
 
 
